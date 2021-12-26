036cec79331b79fc4719776c8099920fc61ad35e
dongrixinyu <dongrixinyu.89@163.com>
2021-12-26 20:55:35 +0800

    update code and zip_file

--- jiojio/util/__init__.py
+++ jiojio/util/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding=utf-8 -*-
 
 from .time_it import TimeIt
-from .zip_file import unzip_file
+from .zip_file import unzip_file, zip_file
 from .file_io import read_file_by_iter, write_file_by_line  # , read_file_by_line,
 from .trie_tree import TrieTree
--- jiojio/util/zip_file.py
+++ jiojio/util/zip_file.py
@@ -10,38 +10,28 @@ FILE_PATH = os.path.abspath(__file__)
 DIR_PATH = os.path.dirname(os.path.dirname(FILE_PATH))
 
 
-def zip_file(file_list=None):
-    """ 将某些 txt, json 文件压缩 """
-    if file_list is None:
-        file_list = UNZIP_FILE_LIST
-    elif type(file_list) is str:
-        file_list = [file_list]
+def zip_file(file_path):
+    """ 将某些 txt, json 文件压缩，须指定绝对路径 """
+    dir_path = os.path.dirname(file_path)
+    file_name = os.path.basename(file_path)
 
-    dict_dir_path = os.path.join(DIR_PATH, 'dictionary')
-    for _file in file_list:
-        dict_file_path = os.path.join(dict_dir_path, _file)
-        tmp_file_path = os.path.join(os.getcwd(), _file)
-        shutil.copyfile(dict_file_path, tmp_file_path)
+    # 必须复制一下
+    tmp_file_path = os.path.join(os.getcwd(), file_name)
+    shutil.copyfile(file_path, tmp_file_path)
 
-        zip_file_name = _file.split('.')[0] + '.zip'
+    zip_file_name = file_name.split('.')[0] + '.zip'
 
-        with zipfile.ZipFile(os.path.join(dict_dir_path, zip_file_name),
-                             'w', zipfile.ZIP_DEFLATED) as zf:
-            # zf.write(os.path.join(dict_dir_path, _file))
-            zf.write(_file)
+    with zipfile.ZipFile(os.path.join(dir_path, zip_file_name),
+            'w', zipfile.ZIP_DEFLATED) as zf:
+        zf.write(file_name)
 
-        os.remove(tmp_file_path)
+    os.remove(tmp_file_path)
 
 
-def unzip_file(file_list):
-    """ 将某些 txt 文件解压缩 """
-    if type(file_list) is str:
-        file_list = [file_list]
-
-    for _zip_file in file_list:
-        dict_dir_path = os.path.dirname(_zip_file)
-        zip_file_path = os.path.join(dict_dir_path, _zip_file)
-        with zipfile.ZipFile(zip_file_path, 'r') as zf:
-            assert len(zf.namelist()) == 1
-            for _file in zf.namelist():
-                zf.extract(_file, dict_dir_path)
+def unzip_file(zip_file_path):
+    """ 将某些 txt 文件解压缩，须指定绝对路径 """
+    zip_dir_path = os.path.dirname(zip_file_path)
+    with zipfile.ZipFile(zip_file_path, 'r') as zf:
+        assert len(zf.namelist()) == 1
+        for _file in zf.namelist():
+            zf.extract(_file, zip_dir_path)
