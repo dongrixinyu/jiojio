83346f41b66222b031c6a2343948c46c19033ea8
dongrixinyu <dongrixinyu.89@163.com>
2022-01-05 17:59:40 +0800

    update c code

--- jiojio/jiojio_cpp/featureExtractor.h
+++ jiojio/jiojio_cpp/featureExtractor.h
@@ -1,5 +1,8 @@
 #include "Python.h"
 #include <stdio.h>
+// #include <stdbool.h>
 #include <locale.h>
+
+inline wchar_t *getSliceStr(wchar_t *text, int start, int length, int all_len);
 
 PyObject *getNodeFeature(int idx, wchar_t *text, int nodeNum);
