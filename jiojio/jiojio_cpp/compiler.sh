79b306f7d7e7aceb18e37f79fd24821ed52a2dbb
dongrixinyu <dongrixinyu.89@163.com>
2022-02-15 10:16:17 +0000

    update cmake and compiler

--- jiojio/jiojio_cpp/compiler.sh
+++ jiojio/jiojio_cpp/compiler.sh
@@ -5,6 +5,11 @@ if [ ! -d build ]; then
 else
     rm -rf build/*
 fi
-cd build && cmake .. && make
 
-cd .. && python test_cpp.py
+cd build
+cmake .. \
+    -DPYTHON_INCLUDE_DIRS=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
+    -DPYTHON_LIBRARIES=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR') + '/' + sysconfig.get_config_var('LDLIBRARY'))")
+make
+
+# cd .. && python test_cpp.py
