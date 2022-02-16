7323b9786ac4d0550f98774170730bc62e97ddf8
dongrixinyu <dongrixinyu.89@163.com>
2022-02-16 03:38:04 +0000

    update setup.py with ctypes and c

--- jiojio/util/__init__.py
+++ jiojio/util/__init__.py
@@ -13,11 +13,18 @@ from .trie_tree import TrieTree
 
 dir_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
                         'jiojio_cpp')
+file_list = os.listdir(os.path.join(dir_path, 'build'))
 
 # load `get_node_features_c`
 try:
+    file_name = ''
+    for _file_name in file_list:
+        if 'libfeatureExtractor' in _file_name and _file_name.endswith('.so'):
+            file_name = _file_name
+            break
+
     feature_extractor = ctypes.PyDLL(
-        os.path.join(dir_path, 'build', 'libfeatureExtractor.so'))
+        os.path.join(dir_path, 'build', file_name))
     get_node_features_c = feature_extractor.getNodeFeature
     get_node_features_c.argtypes = [
         ctypes.c_int, ctypes.c_wchar_p, ctypes.c_int, ctypes.py_object, ctypes.py_object]
@@ -27,13 +34,20 @@ try:
 
 except Exception:
     get_node_features_c = None
-    print('# Failed to load C func `get_node_features_c` {}.'.format(
-        traceback.format_exc()))
+    print('# Failed to load C func `get_node_features_c`, use py func `get_node_features` instead.')
+    # print('# Failed to load C func `get_node_features_c` {}.'.format(
+    #     traceback.format_exc()))
 
 # load `tag_words_converter_c`
 try:
+    file_name = ''
+    for _file_name in file_list:
+        if 'libtagWordsConverter' in _file_name and _file_name.endswith('.so'):
+            file_name = _file_name
+            break
+
     tag_words_converter = ctypes.PyDLL(
-        os.path.join(dir_path, 'build', 'libtagWordsConverter.so'))
+        os.path.join(dir_path, 'build', file_name))
     tag2word_c = tag_words_converter.tagWordsConverter
     # tag2word_c.argtypes = [ctypes.c_wchar_p, ctypes.py_object]
     tag2word_c.restype = ctypes.py_object
@@ -42,5 +56,6 @@ try:
 
 except Exception:
     tag2word_c = None
-    print('# Failed to load C func `tag2word_c` {}.'.format(
-        traceback.format_exc()))
+    print('# Failed to load C func `tag2word_c`, use py func `tag2word` instead.')
+    # print('# Failed to load C func `tag2word_c` {}.'.format(
+    #     traceback.format_exc()))
