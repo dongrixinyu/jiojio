ba7777718344541527b68bc6ec8e8f7907e312a3
dongrixinyu <dongrixinyu.89@163.com>
2022-08-04 23:55:31 +0800

    update model info

---
+++ jiojio/models/default_cws_model/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding=utf-8 -*-
+# Library: jiojio
+# Author: dongrixinyu
+# License: GPL-3.0
+# Email: dongrixinyu.89@163.com
+# Github: https://github.com/dongrixinyu/jiojio
+# Description: fast Chinese Word Segmentation(CWS) and Part of Speech(POS) based on CPU.'
+# Website: http://www.jionlp.com/
+
+
+# 2022-07-17
+# 训练集样本 12,614,343 条，测试集 400,000 条
+# 模型参数共 504 万个。
+# 2022-07-17 21:22:48 INFO train: # train_set:
+# 2022-07-- gold-num=3874874  output-num=3887747  correct-num=3685385
+#         - precision=94.79%  recall=95.11%  f-score=94.95%
+#         - token_acc=97.77%  sample_acc=48.01%
+
+# 2022-07-17 21:25:14 INFO train: # test_set:
+# 2022-07-17 21:31:17 INFO test:  - test-sample-num=400000
+#         - gold-num=12843788  output-num=12881264  correct-num=12224255
+#         - precision=94.90%  recall=95.18%  f-score=95.04%
+#         - token_acc=97.80%  sample_acc=47.71%
