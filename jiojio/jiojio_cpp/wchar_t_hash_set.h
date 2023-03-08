aa3aa0c2b1d403b657087ca1f9b6955716b9c214
dongrixinyu <dongrixinyu.89@163.com>
2023-03-08 12:28:10 +0000

    update pure C jiojio

---
+++ jiojio/jiojio_cpp/wchar_t_hash_set.h
@@ -0,0 +1,37 @@
+#ifndef _WCHAR_T_HASH_SET_H
+#define _WCHAR_T_HASH_SET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <locale.h>
+
+#ifdef _WIN32
+#define API __declspec(dllexport)
+#else
+#define API
+#endif
+
+typedef struct SetHashNode_Struct SetHashNode;
+
+struct SetHashNode_Struct
+{
+    wchar_t *sKey;
+    SetHashNode *pNext;
+};
+
+SetHashNode **set_hash_table_init(int hashTableMaxSize);
+
+unsigned int set_hash_table_hash_str(const wchar_t *skey);
+void set_hash_table_insert(SetHashNode **setHashTable, const wchar_t *skey, int hashTableMaxSize);
+void set_hash_table_remove(SetHashNode **setHashTable, const wchar_t *skey, int hashTableMaxSize);
+int set_hash_table_lookup(SetHashNode **setHashTable, const wchar_t *skey, int hashTableMaxSize);
+void set_hash_table_release(SetHashNode **setHashTable, int hashTableMaxSize);
+
+// statistics
+double kl_divergence(int count_table[], int hashTableMaxSize, int hashTableSize);
+float detect_empty_bucket(int count_table[], int hashTableMaxSize);
+void distribution_statistics(SetHashNode **setHashTable, int hashTableMaxSize, int hashTableSize);
+
+#endif
