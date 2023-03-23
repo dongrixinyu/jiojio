b077940daf9ae3ab0087dc06fe05eaf2a5d8619f
dongrixinyu <dongrixinyu.89@163.com>
2023-03-23 06:25:58 +0000

    delete irrelevant files

--- jiojio/jiojio_cpp/test.py
+++
@@ -1,7 +0,0 @@
-import jiojio
-import pdb
-
-jiojio.init()
-res = jiojio.cut('每一个人')#都是一个觊觎的个体。')
-print(res)
-pdb.set_trace()
--- jiojio/jiojio_cpp/weightCompute.h
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-#include "Python.h"
-#include "numpy/arrayobject.h"
-
-float **initModelWeight(PyObject *pyModelWeightList);
-
-PyObject *computeNodeWeight(float **modelWeightObj, int **featureIdxListPtr, int textLength);
