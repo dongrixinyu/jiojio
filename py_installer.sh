925c25385d958323710d21e905e569b9e7f2dd0e
dongrixinyu <dongrixinyu.89@163.com>
2024-08-13 11:36:20 +0800

    Update py_installer.sh

--- py_installer.sh
+++ py_installer.sh
@@ -20,7 +20,7 @@ echo "jiojio version: ${jiojio_version}"
 
 # should designate python version
 py3_min_version="6"
-py3_max_version="11"
+py3_max_version="12"
 
 orig_python_path=`which python`
 orig_pip_path=`which pip`
